c7cf8ff02d5d75310bbd324bccd1dd49e61c7fb9
SaimJoy <saimjoy10@gmail.com>
2023-09-21 19:37:52 +0600

    opened form

--- 1.Runing_test_browser/1.launch_browser/login_biztraq.py
+++ 1.Runing_test_browser/1.launch_browser/login_biztraq.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+import random
 import time
 
 #driver setup
@@ -50,9 +51,25 @@ time.sleep(5)
 stn_icon= driver.find_element(By.XPATH,
                              "/html/body/div[1]/app-root/div/main-nav/mat-toolbar/mat-toolbar-row/div/button[1]/span[1]/mat-icon" )
 driver.execute_script("arguments[0].click();", stn_icon)  #there is an overlapped element thats why we used this JS method
-time.sleep(7)
+time.sleep(3)
 
 stn_btn=driver.find_element(By.XPATH, '//*[@id="mat-menu-panel-1"]/div/button[1]' )
 stn_btn.click()
-time.sleep(5)
+time.sleep(3)
 
+#locate custom module
+cust_mdl = driver.find_element(By.XPATH, '//*[@id="mat-tab-label-0-2"]/div/span')
+cust_mdl.click()
+time.sleep(3)
+add_mdl = driver.find_element(By.XPATH, '/html/body/div[1]/app-root/div/main-nav/mat-drawer-container/mat-drawer-content/div/app-settings/mat-tab-group/mat-tab-header/div[2]/div/div/div[3]/div[1]/button/span[1]/mat-icon ' )
+add_mdl.click()
+time.sleep(3)
+
+#add new module
+#generate random string for input
+
+random_string = ''.join(random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for _ in range(10))
+
+new_mdl_form = driver.find_element(By.NAME, 'siteName ')
+new_mdl_form.send_keys(random_string)
+time.sleep(4)
