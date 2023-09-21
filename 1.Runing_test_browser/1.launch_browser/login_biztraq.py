59aafdfd5947360372089af586f9e6b365e31874
SaimJoy <saimjoy10@gmail.com>
2023-09-21 18:26:14 +0600

    1.Login 2.Nav to Settings

--- 1.Runing_test_browser/1.launch_browser/login_biztraq.py
+++ 1.Runing_test_browser/1.launch_browser/login_biztraq.py
@@ -13,6 +13,7 @@ import time
 #driver setup
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://biztraqonline.com/Login")
+driver.maximize_window()
 
 #locate element
 username = driver.find_element(By.ID,"mat-input-0")
@@ -24,17 +25,34 @@ username.clear()
 username.send_keys("joy.triulta")
 time.sleep(5)
 
-#nxt_btn= driver.find_element(By. "")
-#if username is not None:
-    #print("User Name Element found")
-#else:
-    #print("not found")
+#locate button element
+nxt_btn= driver.find_element(By.XPATH,
+                             "/html/body/div[1]/app-root/div/main-nav/mat-drawer-container/mat-drawer-content/div/app-login/div[1]/div[1]/p[2]/button")
+nxt_btn.click()
+time.sleep(5)
 
-#nxt_btn.click()
+#locate password box
+u_pass= driver.find_element(By.XPATH,'//*[@id="passwrdInput"]' )
+if username is not None:
+    print("Password Element found")
+else:
+    print("not found")
+time.sleep(5)
 
-#u_pass= driver.find_element(By.XPATH, ""
+u_pass.send_keys("12345678")
+time.sleep(5)
 
+lg_btn=driver.find_element(By.XPATH ," /html/body/div[1]/app-root/div/main-nav/mat-drawer-container/mat-drawer-content/div/app-login/div[1]/div[1]/p[5]/button")
+lg_btn.click()
+time.sleep(5)
 
-time.sleep(10)
+#locate settins element
+stn_icon= driver.find_element(By.XPATH,
+                             "/html/body/div[1]/app-root/div/main-nav/mat-toolbar/mat-toolbar-row/div/button[1]/span[1]/mat-icon" )
+driver.execute_script("arguments[0].click();", stn_icon)  #there is an overlapped element thats why we used this JS method
+time.sleep(7)
 
+stn_btn=driver.find_element(By.XPATH, '//*[@id="mat-menu-panel-1"]/div/button[1]' )
+stn_btn.click()
+time.sleep(5)
 
