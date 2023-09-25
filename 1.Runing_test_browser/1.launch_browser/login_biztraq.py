80aaec4d2d8d8d9964a9c634f6ed91564ff92fae
SaimJoy <saimjoy10@gmail.com>
2023-09-26 01:32:44 +0600

    Form Open

--- 1.Runing_test_browser/1.launch_browser/login_biztraq.py
+++ 1.Runing_test_browser/1.launch_browser/login_biztraq.py
@@ -60,7 +60,7 @@ time.sleep(5)
 
 stn_btn=driver.find_element(By.XPATH, '//*[@id="mat-menu-panel-1"]/div/button[1]' )
 stn_btn.click()
-time.sleep(3)
+time.sleep(4)
 
 #locate custom module
 cust_mdl = driver.find_element(By.XPATH, '//*[@id="mat-tab-label-0-2"]/div/span')
@@ -71,21 +71,31 @@ add_mdl.click()
 time.sleep(3)
 
 #frame locate
-modal = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.ID, "cdk-overlay-1")) )
+modal = WebDriverWait(driver, 12).until( EC.presence_of_element_located((By.ID, "cdk-overlay-1")) )
 # Handle the modal pop-up
 # # You can interact with the elements inside the modal using their locators
 # # For example, you can click a button inside the modal pop-up:
+time.sleep(2)
 Name=modal.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/mat-dialog-container/new-module-add-dialog/div/div/mat-tab-group/div/mat-tab-body/div/form/mat-card/mat-card-content/div[1]/mat-form-field/div/div[1]/div/input')
-def generate_random_string(length=10):
+time.sleep(2)
+Name.send_keys("Test Module ")
+#For random input
+def generate_random_string(length=3):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
 unique_value = generate_random_string()
 Name.send_keys(unique_value)
+time.sleep(2)
+#description
+#descp= driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/mat-dialog-container/new-module-add-dialog/div/div/mat-tab-group/div/mat-tab-body/div/form/mat-card/mat-card-content/div[2]/mat-form-field')
+#time.sleep(3)
+#descp.send_keys("This a test module")
+
 
 sv_btn= driver.find_element(By.XPATH , '/html/body/div[4]/div[2]/div/mat-dialog-container/new-module-add-dialog/div/div/mat-tab-group/div/mat-tab-body/div/form/mat-card-actions/button[1]/span[1]')
 sv_btn.click()
 
-#time.sleep(5)
+time.sleep(5)
 modal_1 = WebDriverWait(driver, 20).until( EC.presence_of_element_located((By.ID, "mat-tab-label-4-1")) )
 driver.execute_script("arguments[0].click();", modal_1)
 #Permission check box
@@ -93,6 +103,7 @@ driver.execute_script("arguments[0].click();", modal_1)
 checkbox_1 = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/mat-dialog-container/new-module-add-dialog/div/div/mat-tab-group/div/mat-tab-body[2]/div/app-user-access-control/mat-card/mat-card-content/mat-checkbox[1]/label/span[2]')
 driver.execute_script("arguments[0].click();", checkbox_1)
 time.sleep(3)
+#ACCESS CHECK BOX
 checkbox_2 = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/mat-dialog-container/new-module-add-dialog/div/div/mat-tab-group/div/mat-tab-body[2]/div/app-user-access-control/mat-card/mat-card-content/mat-checkbox[2]')
 driver.execute_script("arguments[0].click();", checkbox_2)
 time.sleep(3)
@@ -101,7 +112,7 @@ driver.execute_script("arguments[0].click();", submit_button)
 
 time.sleep(10)
 
-#again check the added module is there or no
+#again check the added module is there or not
 
 
 
